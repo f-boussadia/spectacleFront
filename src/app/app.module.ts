0451ec79a1578cfb99ca5684b3fc918e4303e975
AFFES Ines <ines_affes@yahoo.com>
2019-01-29 15:51:15 +0100

    debut de projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyHeaderComponent } from './my-header/my-header.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
+import { MyMenuComponent } from './my-menu/my-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent,
+    MyFooterComponent,
+    MyMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
